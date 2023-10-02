afda663f70c93f0fc046ebce6c8a139a9fcb7423
David Rahabi <davidrahabi@gmail.com>
2023-10-02 04:34:21 -0400

    FIXED NULL LETS GOOOOOOOOOO

--- src/eventorganizer/EventCalendar.java
+++ src/eventorganizer/EventCalendar.java
@@ -74,7 +74,7 @@ public class EventCalendar {
             return false;
         }
 
-        Event[] removeEvent = new Event[size]; //does she want us to decrease array size when removed?
+        Event[] removeEvent = new Event[size];
         int count=0;
         for (int i = 0; i < numEvents; i++){
             if (i != rm){
@@ -102,147 +102,144 @@ public class EventCalendar {
         return eventInArray;
     }
     public void print() {
-        System.out.println("* Event calendar *");
         if (numEvents == 0) {
             System.out.println("Event calender is empty!");
-        } else {
-            for (int i = 0; i < this.numEvents; i++) {
-                System.out.println(events[i].toString());
-            }
-        } //print the array as is
-        System.out.println("* end of event calendar *");
+        }
+        else {
+            System.out.println("* Event calendar *");
+            if (numEvents == 0) {
+                System.out.println("Event calender is empty!");
+            } else {
+                for (int i = 0; i < this.numEvents; i++) {
+                    System.out.println(events[i].toString());
+                }
+            } //print the array as is
+            System.out.println("* end of event calendar *");
+        }
     }
     public void printByDate() {
-        System.out.println("* Event calendar by event date and start time *");
         if (numEvents == 0) {
             System.out.println("Event calender is empty!");
         } else {
+            System.out.println("* Event calendar by event date and start time *");
+            Event[] sortedDate=events.clone();
             for (int i = 0; i < numEvents; i++) { //sorting the events array by date using selection sort
-                Event earliestEvent = events[i];
+                Event earliestEvent = sortedDate[i];
                 int placeToSwitch = i;
                 for (int p = i + 1; p < numEvents; p++) {
-                    if (events[p].compareTo(earliestEvent) < 0) {
-                        earliestEvent = events[p];
+                    if (sortedDate[p].compareTo(earliestEvent) < 0) {
+                        earliestEvent = sortedDate[p];
                         placeToSwitch = p;
                     }
                 }
-                Event temp = events[placeToSwitch];
-                events[placeToSwitch] = events[i];
-                events[i] = temp;
+                Event temp = sortedDate[placeToSwitch];
+                sortedDate[placeToSwitch] = sortedDate[i];
+                sortedDate[i] = temp;
             } // events is now sorted
             for (int i = 0; i < numEvents; i++) {
-                System.out.println(events[i].toString());
+                System.out.println(sortedDate[i].toString());
             }
+            System.out.println("* end of event calendar *");
         }
-        System.out.println("* end of event calendar *");
     }
 
     public void printByCampus() {
-        System.out.println("* Event calendar by campus and building *");
         if (numEvents == 0) {
             System.out.println("Event calender is empty!");
         } else {
-            Event[] sortedEvents = new Event[numEvents]; //making a new array to store the events sorted by campus in alphabetical order
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < numEvents; j++) {
-                    if (i == 0) {
-                        if (events[j].getLocation().getCampus().equals("Busch")) {
-                            for (int z = 0; z < sortedEvents.length; z++) {
-                                if (sortedEvents[z] == null) {
-                                    sortedEvents[z] = events[j];
-                                    break;
-                                }
-                            }
-                        }
-                    } else if (i == 1) {
-                        if (events[j].getLocation().getCampus().equals("College Avenue")) {
-                            for (int z = 0; z < sortedEvents.length; z++) {
-                                if (sortedEvents[z] == null) {
-                                    sortedEvents[z] = events[j];
-                                    break;
-                                }
-                            }
-                        }
-                    } else if (i == 2) {
-                        if (events[j].getLocation().getCampus().equals("Livingston")) {
-                            for (int z = 0; z < sortedEvents.length; z++) {
-                                if (sortedEvents[z] == null) {
-                                    sortedEvents[z] = events[j];
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-            } // sortedEvents is now sorted by campus alphabetically
+            System.out.println("* Event calendar by campus and building *");
+            Event[] sortedEvents = sortByCampus(events); //making a new array to store the events sorted by campus in alphabetical order
+            // sortedEvents is now sorted by campus alphabetically
             for (int i = 0; i < numEvents; i++) {
                 System.out.println(sortedEvents[i].toString());
             }
-        } //ordered by campus and building/room
-        System.out.println("* end of event calendar *");
-    }
+            System.out.println("* end of event calendar *");
+        }
+    } //ordered by campus and building/room
+
 
     public void printByDepartment() {
-        System.out.println("* Event calendar by department *");
+
         if (numEvents == 0) {
             System.out.println("Event calender is empty!");
         } else {
-            Event[] sortedEvents = new Event[numEvents]; //making a new array to store the events sorted by campus in alphabetical order
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < numEvents; j++) {
-                    if (i == 0) {
-                        if (events[j].getContact().getDepartment().getDepartmentName().equals("Business Analytics and Information Technology")) {
-                            for (int z = 0; z < sortedEvents.length; z++) {
-                                if (sortedEvents[z] == null) {
-                                    sortedEvents[z] = events[j];
-                                    break;
-                                }
-                            }
-                        }
-                    } else if (i == 1) {
-                        if (events[j].getContact().getDepartment().getDepartmentName().equals("Computer Science")) {
-                            for (int z = 0; z < sortedEvents.length; z++) {
-                                if (sortedEvents[z] == null) {
-                                    sortedEvents[z] = events[j];
-                                    break;
-                                }
-                            }
-                        }
-                    } else if (i == 2) {
-                        if (events[j].getContact().getDepartment().getDepartmentName().equals("Electrical Engineering")) {
-                            for (int z = 0; z < sortedEvents.length; z++) {
-                                if (sortedEvents[z] == null) {
-                                    sortedEvents[z] = events[j];
-                                    break;
-                                }
-                            }
-                        }
-                    } else if (i == 3) {
-                        if (events[j].getContact().getDepartment().getDepartmentName().equals("Information Technology and Informatics")) {
-                            for (int z = 0; z < sortedEvents.length; z++) {
-                                if (sortedEvents[z] == null) {
-                                    sortedEvents[z] = events[j];
-                                    break;
-                                }
-                            }
-                        }
-                    } else if (i == 4) {
-                        if (events[j].getContact().getDepartment().getDepartmentName().equals("Mathematics")) {
-                            for (int z = 0; z < sortedEvents.length; z++) {
-                                if (sortedEvents[z] == null) {
-                                    sortedEvents[z] = events[j];
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            System.out.println("* Event calendar by department *");
+            Event[] sortedEvents = sortByDepartment(events); //making a new array to store the events sorted by campus in alphabetical order
             for (int i = 0; i < sortedEvents.length; i++) {
                 System.out.println(sortedEvents[i].toString());
             }
+            System.out.println("* end of event calendar *");
+
         } //ordered by department
-        System.out.println("* end of event calendar *");
+
+    }
+
+    public Event[] sortByCampus(Event[] array){
+        Event[] sortedByCampus=new Event[numEvents];
+        int numElements_InSortedEvents = 0;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < numEvents; j++) {
+                if (i == 0 && events[j].getLocation().getCampus().equals("Busch") && events[j].getLocation().getBuildingName().equals("Allison Road Classroom")) {
+                    sortedByCampus[numElements_InSortedEvents] = events[j];
+                    numElements_InSortedEvents++;
+                }
+                else if(i == 1 && events[j].getLocation().getCampus().equals("Busch") && events[j].getLocation().getBuildingName().equals("Hill Center")){
+                    sortedByCampus[numElements_InSortedEvents] = events[j];
+                    numElements_InSortedEvents++;
+                }
+                else if(i==2 && events[j].getLocation().getCampus().equals("College Avenue") && events[j].getLocation().getBuildingName().equals("Academic Building")){
+                    sortedByCampus[numElements_InSortedEvents] = events[j];
+                    numElements_InSortedEvents++;
+                }
+                else if (i == 3 && events[j].getLocation().getCampus().equals("College Avenue") && events[j].getLocation().getBuildingName().equals("Murray Hall")) {
+                    sortedByCampus[numElements_InSortedEvents] = events[j];
+                    numElements_InSortedEvents++;
+                } else if (i == 4 && events[j].getLocation().getCampus().equals("Livingston") && events[j].getLocation().getBuildingName().equals("Beck Hall")) {
+                    sortedByCampus[numElements_InSortedEvents] = events[j];
+                    numElements_InSortedEvents++;
+                }
+                else if (i == 5 && events[j].getLocation().getCampus().equals("Livingston") && events[j].getLocation().getBuildingName().equals("Tillett Hall")){
+                    sortedByCampus[numElements_InSortedEvents] = events[j];
+                    numElements_InSortedEvents++;
+                }
+            }
+        }
+        return sortedByCampus;
+    }
+    public Event[] sortByDepartment(Event[] array){
+        Event[] sortedDepartment=new Event[numEvents];
+        int numElements_InSortedDepartment=0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < numEvents; j++) {
+                if (i == 0) {
+                    if (events[j].getContact().getDepartment().getDepartmentName().equals("Business Analytics and Information Technology")) {
+                        sortedDepartment[numElements_InSortedDepartment] = events[j];
+                        numElements_InSortedDepartment++;
+                    }
+                } else if (i == 1) {
+                    if (events[j].getContact().getDepartment().getDepartmentName().equals("Computer Science")) {
+                        sortedDepartment[numElements_InSortedDepartment] = events[j];
+                        numElements_InSortedDepartment++;
+                    }
+                } else if (i == 2) {
+                    if (events[j].getContact().getDepartment().getDepartmentName().equals("Electrical Engineering")) {
+                        sortedDepartment[numElements_InSortedDepartment] = events[j];
+                        numElements_InSortedDepartment++;
+                    }
+                } else if (i == 3) {
+                    if (events[j].getContact().getDepartment().getDepartmentName().equals("Information Technology and Informatics")) {
+                        sortedDepartment[numElements_InSortedDepartment] = events[j];
+                        numElements_InSortedDepartment++;
+                    }
+                } else if (i == 4) {
+                    if (events[j].getContact().getDepartment().getDepartmentName().equals("Mathematics")) {
+                        sortedDepartment[numElements_InSortedDepartment] = events[j];
+                        numElements_InSortedDepartment++;
+                    }
+                }
+            }
+        }
+        return sortedDepartment;
     }
 
 
